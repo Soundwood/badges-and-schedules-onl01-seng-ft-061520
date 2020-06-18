28281c1611719046af30c342f1c5f59ad4861c60
Ben Hogan <j.benhogan@gmail.com>
2020-06-18 13:19:39 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,9 +3,9 @@ def badge_maker(name)
   "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(array)
+def batch_badge_creator(attendees)
   batch_badges_list = []
-  array.each do |name|
+  attendees.each do |name|
     batch_badges_list << "Hello, my name is #{name}."
   end
   return batch_badges_list
@@ -14,8 +14,8 @@ end
 def assign_rooms(speakers)
   room_assignments = []
   room_count = 1
-  speakers.each do |speaker|
-    room_assignments << "Hello, #{speaker}! You'll be assigned to room #{room_count}!"
+  speakers.each.with_index(1) do |speaker, index|
+    room_assignments << "Hello, #{speaker}! You'll be assigned to room #{index}!"
     room_count += 1
   end
   return room_assignments
